4c62e9e2e8e2de0f45e6c068572e17814cf9214e
molly mk <mmkover@gmail.com>
2020-08-21 03:31:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,8 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.length > 2 
     last = array.pop
-    array.join(", ")
-    array << last
+    fruits = array.join(", ")
+    fruits << last
   else
     array.join
   end
