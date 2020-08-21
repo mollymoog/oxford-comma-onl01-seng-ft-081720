5092778b0e44794819a86d9237dbe49105df4347
molly mk <mmkover@gmail.com>
2020-08-21 03:33:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length > 2 
+    last = array.pop
+    fruits = array.join(", ")
+    fruits << ", and #{last}"
+  else
+    array.join
+  end
 end
